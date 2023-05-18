79eef7971e75c16d463ab9b8cfc64a1bfd227eb8
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-05-18 18:19:02 -0500

    This commit adds some minor CSS and refactors our React routes

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,64 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer/index';
 import Header from './components/Nav/index';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Cart from './components/Cart';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+	uri: '/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem('id_token');
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              {/* Create a route to display a single thought's comments based on its `thoughtId` provided in the URL */}
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-            </Routes>
-          </div>
+	return (
+    <BrowserRouter>
+			<ApolloProvider client={client}>
+				<Routes>
+					<Header />
+								<Route path='/' element={<Home />} />
+								<Route path='/login' element={<Login />} />
+                <Route path='/cart' element={<Cart />} />
           <Footer />
-        </div>
-    </Router>
-    <Footer />
-    </ApolloProvider>
-  );
+				</Routes>
+			</ApolloProvider> 
+    </BrowserRouter>
+
+	);
 }
 
-export default App; 
+export default App;

e46a8f19e6e6b352b26bbd4c522fb6c18e275757
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-05-18 21:26:33 -0500

    test push

--- Develop/client/src/pages/Home.js
+++ Develop/client/src/pages/Home.js
@@ -1,28 +1,14 @@
 import React from 'react';
+import {PRODUCTS} from '../components/ProductItem';
+import ProductList from '../components/ProductList';
 
-export default function Home() {
+const Home = () => {
 	return (
-		<main>
-			<div>
-				<div class='hero-body'>
-					<p class='title'>Welcome</p>
-					<p class='subtitle'>Look at this stuff... isn't it neat 😉</p>
-				</div>
-			</div>
+		<div className='productContainer'>
+			<div className='product'></div>
+			<ProductList products={PRODUCTS}/>
+		</div>
+	)
+};
 
-			<div class='columns is-inline '>
-				<div class='mb-4 mr-4 ml-4 catalog card column is-2 is-inline-flex'>
-					<div class='card-image'>
-						<figure class='image is-3by3'>
-							<img src='{{product.img}}' alt='product' />
-						</figure>
-
-						<div class='card-content'>
-							<div class='content'></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</main>
-	);
-}
+export default Home;

efb8dbde8d60db4d890001b4152b59e63fb6ba19
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-05-18 19:28:05 -0500

    hard coded local host into uri for development purposes and proxy in p.json

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -13,7 +13,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'http://localhost:3001/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header

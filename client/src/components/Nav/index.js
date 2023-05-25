0202ffb9110bdd4857890c33be3c63f23b3df4d0
Amrita Raja <amritachatarpal@gmail.com>
2023-05-25 17:52:38 -0500

    Bulma, css, and added images

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,63 +7,67 @@ export default function Header() {
   function showNav() {
       return (
         <header>
-
-        <nav class='navbar is-dark is-fixed-top'>
-
-
+        <nav class='navbar is-dark is-fixed-top' role="navigation" aria-label="main navigation">
           <div class='navbar-brand'>
             <Link to='/' class='navbar-item'>
               <img className= "Logo" src={Logo} alt='MoMart' />
             </Link>
-  
-            <a
-              href='/'
+            <a href='/'
               role='button'
               class='navbar-burger'
               aria-label='menu'
-              aria-expanded='false'
-            >
+              aria-expanded='false'>
               <span aria-hidden='true'></span>
               <span aria-hidden='true'></span>
               <span aria-hidden='true'></span>
             </a>
           </div>
+
+
           {Auth.loggedIn() ? (
-            <>
-          <Link to='/catalog' class='navbar-item is-hoverable pt-1'>
-            <span class='material-icons '>sell</span>
-          </Link>
-          <div class='navbar-end'>
-            <p class='pt-3'>Hello, {Auth.getUserName()}</p>
-          </div>
-  
-          <div class='navbar-end'>
-            
+        <>
+          <div class="navbar-menu"> 
+            <div class="navbar-start">
+              <Link to='/catalog' class='navbar-item is-hoverable pt-1'>
+              <p>Catalog </p><span class='material-icons'>sell</span>
+              </Link>
             </div>
-            <Link to='/' onClick={() => Auth.logout()} id='logout' class='navbar-item is-hoverable pt-1'>
-            <span class='material-icons icon has-text-danger'>logout</span>
-          </Link> 
-          </> 
-          ) : (
-         <ul className= "flex-row myNav">
-           <Link to='/catalog' class='navbar-item is-hoverable pt-1'>
-            <span class='material-icons'>sell</span>
-          </Link>
-          <li className="mx-1">
+            <div class='navbar-end'>
+              <p class='pt-3 navbar-item'>Hello, {Auth.getUserName()}</p>
+              <div class="navbar-item">
+                <Link to='/' onClick={() => Auth.logout()} id='logout' class='navbar-item is-hoverable pt-1'>
+                <span class='material-icons icon has-text-danger'>logout</span></Link> 
+              </div>
+            </div>
+          </div>
+          </> ) : (
+
+            
+         <div className= "navbar-menu ">
+            <div class='navbar-end'>
+              <Link to='/catalog' class='navbar-item is-hoverable pt-1'>
+              <span class='material-icons'>sell</span>
+              </Link>
+              <Link to='/login' class='navbar-item is-hoverable pt-1'>
+              <span class='material-icons'>account_circle</span>
+              </Link>
+            </div>
+          
+
+   {/* <li className="mx-1">
           <Link to="/signup">
               Signup
             </Link>
-          </li>
-          <Link to='/login' class='navbar-item is-hoverable pt-1'>
-            <span class='material-icons'>account_circle</span>
-          </Link>
-         </ul>
-          )
-  } 
+          </li> */}
+
+         </div>
+          )}
       </nav>
     </header>
     )
   }
+
+
   return (
       <nav>
         {showNav()}

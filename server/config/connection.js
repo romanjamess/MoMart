01d4c70944f2f19947e22dc4a3b088c1b7a98912
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-05-25 20:18:05 -0500

    deploying to heroku (#67)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+const connectTo =  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/thingamabobs';
+console.log("mongo connection is right here =>", connectTo)
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/thingamabobs'
+  connectTo 
 );
 
 module.exports = mongoose.connection;

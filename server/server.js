bd883369f14eb4a0f9a7bba57fda6d7a90d576e2
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-05-25 20:16:30 -0500

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require("./utils/auth")
